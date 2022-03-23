503d16e2256adec7bf22986bdca03a902260a80f
Julian Roeland <julianroeland@gmail.com>
2022-03-23 19:31:40 +0100

    s

--- src/components/FrontPage/MatchHistory.js
+++ src/components/FrontPage/MatchHistory.js
@@ -4,7 +4,6 @@ import React from "react";
 import EachParticipant from "../MatchHistoryData/EachParticipant";
 
 const MatchHistory = (props) => {
-    console.log(props.matchHistory);
 
     return (
 
