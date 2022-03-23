98c97e21179e69c41868a9c69bcaf4d0165ef84c
Julian Roeland <julianroeland@gmail.com>
2022-03-24 00:40:56 +0100

    &apos on footer <p>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
     return (
         <FooterStyled>
             <footer>
-                <p>Summoner Lookup isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot
-                    Games
+                <p>Summoner Lookup isn&apost endorsed by Riot Games and doesn&apost reflect the views or opinions of
+                    Riot Games
                     or
                     anyone officially involved in producing or managing Riot Games properties.
                     Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games,
