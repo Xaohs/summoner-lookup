a0cc69bf30a8b0241065b3dc94f292e97fdcbe5e
Julian Roeland <julianroeland@gmail.com>
2022-03-29 10:39:31 +0200

    broken json

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
 import Head from 'next/head';
 
 import SummonerLookup from "../src/components/FrontPage/SummonerLookup"
+const fs = require('fs');
+
+export async function getServerSideProps() {
+    fs.readFile('summonerData.json', 'utf-8', (err, jsonString) => {
+        const data = JSON.parse(jsonString)
+        const f = {
+            "yep": "yep",
+            "nop": "nop",
+        }
+        return {
+            props: {dat: f}
+        }
+
+    })
+}
+
 export default function Home() {
+
     return (
         <div>
             <Head>
